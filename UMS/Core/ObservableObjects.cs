4a101700ed17e4aef3e935ba698b2d79421b2bf4
Johhan <Johhanparra1@gmail.com>
2023-10-29 16:28:06 -0500

    Observable Objects Class Added

---
+++ UMS/Core/ObservableObjects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMS.Core
+{
+    internal class ObservableObjects : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnpropertyChanged([CallerMemberName] string name = null) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
