26edf88e942ced6394fe473148e506acfec3dd3a
Johhan <Johhanparra1@gmail.com>
2023-10-29 16:23:30 -0500

    OpenDbConnection Class Added

---
+++ UMS/Models/ModelsDB/OpenDbConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace UMS.Models.ModelsDB
+{
+    internal class OpenDbConnection
+    {
+        public OpenDbConnection() { }
+        public SqlConnection openConnection()
+        {
+            string stringConnection = ConfigurationManager.ConnectionStrings["StringConnection"].ToString();
+
+            var connection = new SqlConnection(stringConnection);
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
