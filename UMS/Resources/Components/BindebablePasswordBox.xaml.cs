eb6efea67b7ad2b3ae5bfd927dd8841802633ffa
MagicExist <johhanparra1@gmail.com>
2023-10-31 22:49:10 -0500

    Added BindeabablePasswordBox Component

---
+++ UMS/Resources/Components/BindebablePasswordBox.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UMS.Resources.Components
+{
+    /// <summary>
+    /// Interaction logic for BindebablePasswordBox.xaml
+    /// </summary>
+    public partial class BindebablePasswordBox : UserControl
+    {
+
+
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Password.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(string), typeof(BindebablePasswordBox), new PropertyMetadata(string.Empty));
+
+
+        public BindebablePasswordBox()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = passwordBox.Password;
+        }
+    }
+}
