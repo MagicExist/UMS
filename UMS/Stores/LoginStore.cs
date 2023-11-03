8de138562f1f2465c1269ed5782b1dd0bf74259c
MagicExist <johhanparra1@gmail.com>
2023-11-02 19:41:03 -0500

    New LoginStore Added

---
+++ UMS/Stores/LoginStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMS.Stores
+{
+    internal class LoginStore
+    {
+        public event Action<object> OnLoginAllow;
+        public void OnLoginAllowInvoke(object newView) 
+        {
+            OnLoginAllow?.Invoke(newView);
+        }
+    }
+}
