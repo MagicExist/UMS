385b09b910e3819cbfa48e74805c88346a504780
kevin Rogers <rogerscardonakevin@gmail.com>
2023-11-04 21:55:21 -0500

    The DataGrid style was created and applied to the list of requests in the support view

---
+++ UMS/Models/Request.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMS.Models
+{
+    public class Request
+    {
+        string _date;
+        string _subject;
+        string _admin;
+        string _status;
+
+        public string Date { get => _date; }
+        public string Subject { get => _subject; }
+        public string Admin { get => _admin; }
+        public string Status { get => _status; }
+
+        public Request(string date, string subject, string admin, string status)
+        {
+            _date = date;
+            _subject = subject;
+            _admin = admin;
+            _status = status;
+        }
+
+
+
+    }
+}
