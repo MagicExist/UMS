7a68eea287f21c099a10e0d915c0ff55cffe2dc9
kevin Rogers <rogerscardonakevin@gmail.com>
2023-11-12 16:01:26 -0500

    Adjustments were made to the design of the classroom search view and code was added to the view model for the advanced search.

--- UMS/ViewModels/SearchClassRoomVM.cs
+++ UMS/ViewModels/SearchClassRoomVM.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using UMS.Core;
 
 namespace UMS.ViewModels
 {
-    class SearchClassRoomVM
+    class SearchClassRoomVM : ObservableObjects
     {
+        #region variables for interface management
+        private Visibility _advancedSearchVisibility;
+
+        public Visibility AdvancedSearchVisibility 
+        {
+            get { return _advancedSearchVisibility; }
+            set 
+            { _advancedSearchVisibility = value; OnpropertyChanged(); } 
+        }
+
+        #endregion
+
+        #region commands
+        public RelayCommand AdvancedSearchCommand { get; set; }
+
+        #endregion
+
+        public SearchClassRoomVM()
+        {
+            AdvancedSearchVisibility = Visibility.Collapsed;
+            AdvancedSearchCommand = new RelayCommand(ShowAdvancedSearch);
+
+        }
+
+        #region Execute Methods
+
+        /// <summary>
+        /// This method is used to show or hide the advanced search section.
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void ShowAdvancedSearch(object parameter) 
+        {
+            if (AdvancedSearchVisibility == Visibility.Collapsed)
+            {
+                AdvancedSearchVisibility= Visibility.Visible;
+            }
+            else 
+            { 
+                AdvancedSearchVisibility= Visibility.Collapsed;
+            }
+        }
+
+        #endregion
+
+
     }
 }
