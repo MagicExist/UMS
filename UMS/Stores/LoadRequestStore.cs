2249dacc964a1756875ab617f17119305aedc36b
MagicExist <johhanparra1@gmail.com>
2023-11-15 01:52:38 -0500

    Stores comments added

--- UMS/Stores/LoadRequestStore.cs
+++ UMS/Stores/LoadRequestStore.cs
@@ -9,7 +9,16 @@ namespace UMS.Stores
 {
     internal class LoadRequestStore
     {
+        /// <summary>
+        /// This event will send the current user in the session along with the user type, enabling the loading of their respective requests.
+        /// </summary>
         public event Action<User,int> OnLoadRequest;
+        /// <summary>
+        /// Invokes the OnLoadRequest event, sending the current user in the session along with the user type.
+        /// This enables the loading of requests specific to the user.
+        /// </summary>
+        /// <param name="currentUser">The current user in the session.</param>
+        /// <param name="currentUserType">The type of the current user.</param>
         public void OnLoadRequestInvoke(User currentUser,int currentUserType) 
         {
             OnLoadRequest?.Invoke(currentUser,currentUserType);
--- UMS/Stores/LoginStore.cs
+++ UMS/Stores/LoginStore.cs
@@ -9,7 +9,17 @@ namespace UMS.Stores
 {
     internal class LoginStore
     {
+
+        /// <summary>
+        /// This event will be used to switch the view between the loginVM and MainVM based on the current user.
+        /// </summary>
         public event Action<object,User> OnLoginAllow;
+
+        /// <summary>
+        /// Invokes the OnLoginAllow event to switch the view between the loginVM and MainVM based on the current user.
+        /// </summary>
+        /// <param name="newView">The new view to be displayed.</param>
+        /// <param name="currentUser">The current user triggering the view switch.</param>
         public void OnLoginAllowInvoke(object newView,User currentUser) 
         {
             OnLoginAllow?.Invoke(newView,currentUser);
